f43b8ab7c9245423e71af26f06ac8ef719ae6071
Thomas Grainger <tom@procensus.com>
2017-10-04 20:02:42 +0100

    eslint fix: no-use-before-define

--- mocha-multi.js
+++ mocha-multi.js
@@ -9,13 +9,203 @@ const mkdirp = require('mkdirp');
 // Let mocha decide about tty early
 require('mocha/lib/reporters/base');
 
-module.exports = MochaMulti;
-
 // Make sure we don't lose these!
 const { stdout, stderr } = process;
 
 function defineGetter(obj, prop, get) {
   Object.defineProperty(obj, prop, { get });
+}
+
+function awaitStreamsOnExit(streams) {
+  const { exit } = process;
+  let num = streams.length;
+  process.exit = (code) => {
+    const quit = exit.bind(process, code);
+    function onClose() {
+      if (num === 0) {
+        quit();
+      }
+    }
+    streams.forEach((stream) => {
+      stream.end(() => {
+        num -= 1;
+        onClose();
+      });
+    });
+  };
+}
+
+function identity(x) {
+  return x;
+}
+
+const msgs = {
+  no_definitions: 'reporter definitions should be set in ' +
+                  'the `multi` shell variable\n' +
+                  "eg. `multi='dot=- xunit=file.xml' mocha`",
+  invalid_definition: "'%s' is an invalid definition\n" +
+                      'expected <reporter>=<destination>',
+  invalid_reporter: "Unable to find '%s' reporter",
+};
+function bombOut(id, ...args) {
+  const newArgs = [`ERROR: ${msgs[id]}`, ...args];
+  stderr.write(`${util.format(...newArgs)}\n`);
+  process.exit(1);
+}
+
+function parseReporter(definition) {
+  const pair = definition.split('=');
+  if (pair.length !== 2) {
+    bombOut('invalid_definition', definition);
+  }
+  return pair;
+}
+
+function parseSetup() {
+  const reporterDefinition = process.env.multi || '';
+  const reporterDefs = reporterDefinition.trim().split(/\s/).filter(identity);
+  if (!reporterDefs.length) {
+    bombOut('no_definitions');
+  }
+  debug('Got reporter defs: %j', reporterDefs);
+  return reporterDefs.map(parseReporter);
+}
+
+function resolveStream(destination) {
+  if (destination === '-') {
+    debug("Resolved stream '-' into stdout and stderr");
+    return null;
+  }
+  debug("Resolved stream '%s' into writeable file stream", destination);
+  // Create directory if not existing
+  const destinationDir = path.dirname(destination);
+  if (!fs.existsSync(destinationDir)) {
+    mkdirp.sync(destinationDir);
+  }
+
+  // Ensure we can write here
+  fs.writeFileSync(destination, '');
+  return fs.createWriteStream(destination);
+}
+
+function safeRequire(module) {
+  try {
+    return require(module);
+  } catch (err) {
+    if (!/Cannot find/.exec(err.message)) {
+      throw err;
+    }
+    return null;
+  }
+}
+
+function resolveReporter(name) {
+  // Cribbed from Mocha.prototype.reporter()
+  let reporter;
+  reporter = safeRequire(`mocha/lib/reporters/${name}`);
+  if (!reporter) {
+    reporter = safeRequire(name);
+  }
+  if (!reporter) {
+    bombOut('invalid_reporter', name);
+  }
+  debug("Resolved reporter '%s' into '%s'", name, util.inspect(reporter));
+  return reporter;
+}
+
+function withReplacedStdout(stream, func) {
+  if (!stream) {
+    func();
+    return;
+  }
+
+  // The hackiest of hacks
+  debug('Replacing stdout');
+
+  const stdoutGetter = Object.getOwnPropertyDescriptor(process, 'stdout').get;
+  const stderrGetter = Object.getOwnPropertyDescriptor(process, 'stderr').get;
+
+  console._stdout = stream;
+  console._stderr = stream;
+  defineGetter(process, 'stdout', () => stream);
+  defineGetter(process, 'stderr', () => stream);
+
+  try {
+    func();
+  } finally {
+    console._stdout = stdout;
+    console._stderr = stderr;
+    defineGetter(process, 'stdout', stdoutGetter);
+    defineGetter(process, 'stderr', stderrGetter);
+    debug('stdout restored');
+  }
+}
+
+function createRunnerShim(runner, stream) {
+  const shim = new (require('events').EventEmitter)();
+
+  function addDelegate(prop) {
+    defineGetter(shim, prop, () => {
+      let property = runner[prop];
+      if (typeof property === 'function') {
+        property = property.bind(runner);
+      }
+      return property;
+    });
+  }
+
+  addDelegate('grepTotal');
+  addDelegate('suite');
+  addDelegate('total');
+
+  const delegatedEvents = {};
+
+  shim.on('newListener', (event) => {
+    if (event in delegatedEvents) return;
+
+    delegatedEvents[event] = true;
+    debug("Shim: Delegating '%s'", event);
+
+    runner.on(event, (...eventArgs) => {
+      eventArgs.unshift(event);
+
+      withReplacedStdout(stream, () => {
+        shim.emit(...eventArgs);
+      });
+    });
+  });
+
+  return shim;
+}
+
+function initReportersAndStreams(runner, setup, multi) {
+  return setup.map((definition) => {
+    const reporter = definition[0];
+    const outstream = definition[1];
+    const options = definition[2];
+
+    debug("Initialising reporter '%s' to '%s' with options %j", reporter, outstream, options);
+
+    const stream = resolveStream(outstream);
+    const shim = createRunnerShim(runner, stream);
+
+    debug("Shimming runner into reporter '%s' %j", reporter, options);
+
+    withReplacedStdout(stream, () => {
+      const Reporter = resolveReporter(reporter);
+      const r = new Reporter(shim, assign({}, multi.options, {
+        reporterOptions: options || {},
+      }));
+      // If the reporter possess a done() method register it so we can
+      // wait for it to complete when done.
+      if (r && r.done) {
+        multi.reportersWithDone.push(r);
+      }
+      return r;
+    });
+
+    return stream;
+  });
 }
 
 function MochaMulti(runner, options) {
@@ -78,194 +268,4 @@ MochaMulti.prototype.done = function (failures, fn) {
   }
 };
 
-const msgs = {
-  no_definitions: 'reporter definitions should be set in ' +
-                  'the `multi` shell variable\n' +
-                  "eg. `multi='dot=- xunit=file.xml' mocha`",
-  invalid_definition: "'%s' is an invalid definition\n" +
-                      'expected <reporter>=<destination>',
-  invalid_reporter: "Unable to find '%s' reporter",
-};
-function bombOut(id, ...args) {
-  const newArgs = [`ERROR: ${msgs[id]}`, ...args];
-  stderr.write(`${util.format(...newArgs)}\n`);
-  process.exit(1);
-}
-
-function parseSetup() {
-  const reporterDefinition = process.env.multi || '';
-  const reporterDefs = reporterDefinition.trim().split(/\s/).filter(identity);
-  if (!reporterDefs.length) {
-    bombOut('no_definitions');
-  }
-  debug('Got reporter defs: %j', reporterDefs);
-  return reporterDefs.map(parseReporter);
-}
-
-function parseReporter(definition) {
-  const pair = definition.split('=');
-  if (pair.length !== 2) {
-    bombOut('invalid_definition', definition);
-  }
-  return pair;
-}
-
-function initReportersAndStreams(runner, setup, multi) {
-  return setup.map((definition) => {
-    const reporter = definition[0];
-    const outstream = definition[1];
-    const options = definition[2];
-
-    debug("Initialising reporter '%s' to '%s' with options %j", reporter, outstream, options);
-
-    const stream = resolveStream(outstream);
-    const shim = createRunnerShim(runner, stream);
-
-    debug("Shimming runner into reporter '%s' %j", reporter, options);
-
-    withReplacedStdout(stream, () => {
-      const Reporter = resolveReporter(reporter);
-      const r = new Reporter(shim, assign({}, multi.options, {
-        reporterOptions: options || {},
-      }));
-      // If the reporter possess a done() method register it so we can
-      // wait for it to complete when done.
-      if (r && r.done) {
-        multi.reportersWithDone.push(r);
-      }
-      return r;
-    });
-
-    return stream;
-  });
-}
-
-function awaitStreamsOnExit(streams) {
-  const { exit } = process;
-  let num = streams.length;
-  process.exit = (code) => {
-    const quit = exit.bind(process, code);
-    streams.forEach((stream) => {
-      stream.end(() => {
-        num -= 1;
-        onClose();
-      });
-    });
-    function onClose() {
-      if (num === 0) {
-        quit();
-      }
-    }
-  };
-}
-
-function resolveReporter(name) {
-  // Cribbed from Mocha.prototype.reporter()
-  let reporter;
-  reporter = safeRequire(`mocha/lib/reporters/${name}`);
-  if (!reporter) {
-    reporter = safeRequire(name);
-  }
-  if (!reporter) {
-    bombOut('invalid_reporter', name);
-  }
-  debug("Resolved reporter '%s' into '%s'", name, util.inspect(reporter));
-  return reporter;
-}
-
-function safeRequire(module) {
-  try {
-    return require(module);
-  } catch (err) {
-    if (!/Cannot find/.exec(err.message)) {
-      throw err;
-    }
-    return null;
-  }
-}
-
-function resolveStream(destination) {
-  if (destination === '-') {
-    debug("Resolved stream '-' into stdout and stderr");
-    return null;
-  }
-  debug("Resolved stream '%s' into writeable file stream", destination);
-  // Create directory if not existing
-  const destinationDir = path.dirname(destination);
-  if (!fs.existsSync(destinationDir)) {
-    mkdirp.sync(destinationDir);
-  }
-
-  // Ensure we can write here
-  fs.writeFileSync(destination, '');
-  return fs.createWriteStream(destination);
-}
-
-function createRunnerShim(runner, stream) {
-  const shim = new (require('events').EventEmitter)();
-
-  addDelegate('grepTotal');
-  addDelegate('suite');
-  addDelegate('total');
-
-  function addDelegate(prop) {
-    defineGetter(shim, prop, () => {
-      let property = runner[prop];
-      if (typeof property === 'function') {
-        property = property.bind(runner);
-      }
-      return property;
-    });
-  }
-
-  const delegatedEvents = {};
-
-  shim.on('newListener', (event) => {
-    if (event in delegatedEvents) return;
-
-    delegatedEvents[event] = true;
-    debug("Shim: Delegating '%s'", event);
-
-    runner.on(event, (...eventArgs) => {
-      eventArgs.unshift(event);
-
-      withReplacedStdout(stream, () => {
-        shim.emit(...eventArgs);
-      });
-    });
-  });
-
-  return shim;
-}
-
-function withReplacedStdout(stream, func) {
-  if (!stream) {
-    func();
-    return;
-  }
-
-  // The hackiest of hacks
-  debug('Replacing stdout');
-
-  const stdoutGetter = Object.getOwnPropertyDescriptor(process, 'stdout').get;
-  const stderrGetter = Object.getOwnPropertyDescriptor(process, 'stderr').get;
-
-  console._stdout = stream;
-  console._stderr = stream;
-  defineGetter(process, 'stdout', () => stream);
-  defineGetter(process, 'stderr', () => stream);
-
-  try {
-    func();
-  } finally {
-    console._stdout = stdout;
-    console._stderr = stderr;
-    defineGetter(process, 'stdout', stdoutGetter);
-    defineGetter(process, 'stderr', stderrGetter);
-    debug('stdout restored');
-  }
-}
-
-function identity(x) {
-  return x;
-}
+module.exports = MochaMulti;
