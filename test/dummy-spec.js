d14ae3ed343f88ce60327c7e2fd76d4a87d00559
Thomas Grainger <tom@procensus.com>
2017-10-04 18:01:37 +0100

    eslint autofix

--- test/dummy-spec.js
+++ test/dummy-spec.js
@@ -1,12 +1,12 @@
-describe('A test', function() {
-  it("isn't really a test", function() {
+describe('A test', () => {
+  it("isn't really a test", () => {
     1 + 1;
-  })
-  it("is only here to create output", function() {
+  });
+  it('is only here to create output', () => {
     2 + 2;
-  })
-  it("runs through and generates data", function() {
+  });
+  it('runs through and generates data', () => {
     throw new Error('to check that reporting is sorta working');
-  })
-  it("handles pending stuff as well")
-})
+  });
+  it('handles pending stuff as well');
+});
--- verify.js
+++ verify.js
@@ -1,66 +1,67 @@
-var Mocha = require('mocha'),
-    should=require('should'),
-    fs=require('fs'),
-    debug = require('debug')('mocha:verify:multi'),
-    async = require('async'),
-    chalk = require('chalk');
+let Mocha = require('mocha'),
+  should = require('should'),
+  fs = require('fs'),
+  debug = require('debug')('mocha:verify:multi'),
+  async = require('async'),
+  chalk = require('chalk');
 
-var reporters = [
-    "dot", "doc", "spec", "json", "progress",
-    "list", "tap", "landing", "xunit", "min",
-    "json-stream", "markdown", "nyan"
-], now = new Date();
+let reporters = [
+    'dot', 'doc', 'spec', 'json', 'progress',
+    'list', 'tap', 'landing', 'xunit', 'min',
+    'json-stream', 'markdown', 'nyan',
+  ],
+  now = new Date();
 
-var reportersWithOptions = []
-    .concat(reporters.map(function (reporter) {
-        var outFilename = tempName(reporter + '-stdout');
-        var options = {};
-        options[reporter] = {
-            stdout: outFilename
-        };
-        return {
-            testName: reporter + ' (with options.stdout)',
-            outFilename: outFilename,
-            options: options
-        };
-    }))
-    .concat(reporters.map(function (reporter) {
-        var outFilename = tempName(reporter + '-str');
-        var options = {};
-        options[reporter] = outFilename;
-        return {
-            testName: reporter + ' (with options as string)',
-            outFilename: outFilename,
-            options: options
-        };
-    }));
+const reportersWithOptions = []
+  .concat(reporters.map((reporter) => {
+    const outFilename = tempName(`${reporter}-stdout`);
+    const options = {};
+    options[reporter] = {
+      stdout: outFilename,
+    };
+    return {
+      testName: `${reporter} (with options.stdout)`,
+      outFilename,
+      options,
+    };
+  }))
+  .concat(reporters.map((reporter) => {
+    const outFilename = tempName(`${reporter}-str`);
+    const options = {};
+    options[reporter] = outFilename;
+    return {
+      testName: `${reporter} (with options as string)`,
+      outFilename,
+      options,
+    };
+  }));
 
 
 should(process.env.multi).not.be.ok;
 
 function tempName(reporter) {
-    return "/tmp/mocha-multi." + reporter + "." + (+now);
+  return `/tmp/mocha-multi.${reporter}.${+now}`;
 }
 
 process.setMaxListeners(reportersWithOptions.length);
 
-async.eachSeries(reportersWithOptions, function(reporter, next) {
-    debug("reporter %s", reporter.testName);
-    debug("reporterOptions %j", reporter.options);
-    var mocha = new Mocha({
-        ui: 'bdd',
-        reporter: "mocha-multi",
-        reporterOptions: reporter.options
-    });
-    mocha.addFile("test/dummy-spec.js");
-    mocha.run(function onRun(){
-        debug("done running %j", reporter.testName);
-        process.nextTick(next);
-    });
-}, function() {
-    reportersWithOptions.forEach(function(reporter) {
-        fs.statSync.bind(fs, reporter.outFilename).should.not.throw();
-        fs.unlinkSync(reporter.outFilename);
-        console.log(chalk.green("%s OK"), reporter.testName);
-    });
+async.eachSeries(reportersWithOptions, (reporter, next) => {
+  debug('reporter %s', reporter.testName);
+  debug('reporterOptions %j', reporter.options);
+  const mocha = new Mocha({
+    ui: 'bdd',
+    reporter: 'mocha-multi',
+    reporterOptions: reporter.options,
+  });
+  mocha.addFile('test/dummy-spec.js');
+  mocha.run(() => {
+    debug('done running %j', reporter.testName);
+    process.nextTick(next);
+  });
+}, () => {
+  reportersWithOptions.forEach((reporter) => {
+    fs.statSync.bind(fs, reporter.outFilename).should.not.throw();
+    fs.unlinkSync(reporter.outFilename);
+    console.log(chalk.green('%s OK'), reporter.testName);
+  });
 });
